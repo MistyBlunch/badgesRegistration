3d273bde8abe41461b49368c9d89e1f80af12494
MistyBlunch <gracenikole@gmail.com>
2019-12-24 02:20:32 -0500

    :sunglasses: one page with a bage created

--- src/index.js
+++ src/index.js
@@ -3,15 +3,8 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 
 import './global.css'
-import Badge from './components/Badge'
+import BadgeNew from './pages/BadgeNew'
 
 const container = document.getElementById('app')
 
-ReactDOM.render(
-<Badge 
-  firstName="Grace" 
-  lastName="Alvarado"
-  avatarUrl="https://avatars2.githubusercontent.com/u/29315728?s=400&u=641b014614edcc6eef166c51579d76eea1d31a4b&v=4"
-  jobTitle="Junior dev"
-  twitter="MistyBlunch"
-  />, container)
+ReactDOM.render(<BadgeNew />, container)
---
+++ src/pages/BadgeNew.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import "./styles/BadgeNew.css";
+import header from "../images/badge-header.svg";
+import Navbar from "../components/Navbar";
+import Badge from "../components/Badge";
+
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName="Grace"
+                lastname="Alvarado"
+                twitter="MistyBlunch"
+                jobTitle="Junior dev"
+                avatarUrl="https://avatars2.githubusercontent.com/u/29315728?s=400&u=641b014614edcc6eef166c51579d76eea1d31a4b&v=4"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BadgeNew;
