e8ec1b0362bbed02be25ab10f63926e1fe0a6866
MistyBlunch <gracenikole@gmail.com>
2019-12-24 00:45:16 -0500

    :squirrel: props in components

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -4,27 +4,27 @@ import './styles/Badge.css'
 import confLogo from '../images/badge-header.svg'
 
 class Badge extends React.Component {
-  // render define el resultado que veremos en pantalla
   render() {
     return (
       <div className="Badge">
         <div className="Badge__header">
-          {/* El scr se llama prop */}
           <img src={confLogo} alt="logo de la conferencia" />
         </div>
 
         <div className="Badge__section-name">
           <img 
             className="Badge__avatar" 
-            src="https://avatars2.githubusercontent.com/u/29315728?s=400&u=641b014614edcc6eef166c51579d76eea1d31a4b&v=4" 
+            src={this.props.avatarUrl}
             alt="Avatar" 
           />
-          <h1>Grace <br/> Alvarado</h1>
+          <h1>
+            {this.props.firstName} <br/> {this.props.lastName}
+          </h1>
         </div>
 
         <div className="Badge__section-info">
-          <h3>Frontend Dev</h3>
-          <div>@gracenikole</div>
+          <h3>{this.props.jobTitle}</h3>
+          <div>@{this.props.twitter}</div>
         </div>
 
         <div className="Badge__footer">
--- src/index.js
+++ src/index.js
@@ -7,4 +7,11 @@ import Badge from './components/Badge'
 
 const container = document.getElementById('app')
 
-ReactDOM.render(<Badge />, container)
+ReactDOM.render(
+<Badge 
+  firstName="Grace" 
+  lastName="Alvarado"
+  avatarUrl="https://avatars2.githubusercontent.com/u/29315728?s=400&u=641b014614edcc6eef166c51579d76eea1d31a4b&v=4"
+  jobTitle="Junior dev"
+  twitter="MistyBlunch"
+  />, container)
