629c4bddca8097bb012261069132e88616403a26
MistyBlunch <gracenikole@gmail.com>
2020-01-05 23:06:45 -0500

    Updates

---
+++ precache-manifest.5c3bbee6305e8bfae0a9c427f4075ba7.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "22300238c2d366fcfa20",
+    "url": "/badgesRegistration/static/css/main.7205fc13.chunk.css"
+  },
+  {
+    "revision": "22300238c2d366fcfa20",
+    "url": "/badgesRegistration/static/js/main.22300238.chunk.js"
+  },
+  {
+    "revision": "8d0e1c3676a09e8df552",
+    "url": "/badgesRegistration/static/js/runtime~main.8d0e1c36.js"
+  },
+  {
+    "revision": "505110dd72895654275b",
+    "url": "/badgesRegistration/static/css/2.3eb6cd4b.chunk.css"
+  },
+  {
+    "revision": "505110dd72895654275b",
+    "url": "/badgesRegistration/static/js/2.505110dd.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/badgesRegistration/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/badgesRegistration/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/badgesRegistration/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/badgesRegistration/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/badgesRegistration/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "6cb5dee0495ca80e7085b891ef6721da",
+    "url": "/badgesRegistration/static/media/NotFoundGif.6cb5dee0.gif"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/badgesRegistration/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/badgesRegistration/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/badgesRegistration/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/badgesRegistration/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/badgesRegistration/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/badgesRegistration/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/badgesRegistration/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/badgesRegistration/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/badgesRegistration/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/badgesRegistration/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/badgesRegistration/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/badgesRegistration/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/badgesRegistration/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/badgesRegistration/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/badgesRegistration/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "7f21cd5dcde7b0232347a5244bd9425b",
+    "url": "/badgesRegistration/index.html"
+  }
+];
